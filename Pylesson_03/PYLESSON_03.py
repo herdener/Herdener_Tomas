9f88171b8c885853d633d8afec1b06fcf599cec2
herdener <tomasherdener@gmail.com>
2016-10-17 10:25:35 -0700

    lab_03

--- Pylesson_03/PYLESSON_03.py
+++ Pylesson_03/PYLESSON_03.py
@@ -5,7 +5,7 @@ print(".....")
 computer = input("hi I like you to answer some questions")
 computer = input("what is your name?")
 
-print(computer , "hi my name is tomas I dont have a nickname so you can call me that")
+print(computer , "that is m name I dont have a nickname so you can call me that")
 
 computer = input("tomas?, without H?,oooooh you are from another counrty.")
 
@@ -14,5 +14,62 @@ computer = input("ok,soo then how old are you?")
 print(computer , "well, now I'am 16")
 
 computer = input("really?, you look younger than that")
+
+computer = input("what kind of music do you like")
+
+print(computer , "I hear mostly everything except reggie")
+
+computer = input("yeah, I dont like reggie nethier")
+
+computer = input("how many siblings do you have")
+
+print(computer, "if you mean my brothers and sisters I have 2")
+
+computer = input("well thats normal, soo what do you do for fun")
+
+print(computer , "well I normally play videogames with my frinds or just go outside and run")
+
+computer = input("and what do you want to be when you grow up?")
+
+print(computer , "still dont know, but I have some options that I like to see first")
+
+computer = input("well then, I hope that you find out what you whant to do, thank for the answers")
+
+
+
+print("EX_02")
+
+height = int(input("please enter your height: "))
+weight = int(input("please enter your weight: "))
+
+BMI = weight  height
+
+print("your BMI is", BMI)
+
+
+print("EX_03")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- Pylesson_05/Pylab_05.py
+++ Pylesson_05/Pylab_05.py
@@ -1,12 +1,24 @@
 import random
 
-num = random.randint(1, 6)
-print(num)
+player = int(input(random.randit(1, 6))
+computer = int(input(random.randit(1, 6))
+
+run rollDice()
+    if player > computer
+    
+
+print("you rolled a <your roll>")
+print("computer rolled a ,computer's roll>")
+print("the winner is <winner>")
+
+
+               
 
 
 
-
-
+             
+               
+             
 
 
 
